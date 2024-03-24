753dfa51eb27a14ed40bf64c08e6593a664873b8
ankit10000 <ankitjangid9197@gmail.com>
2024-03-24 21:23:27 +0530

    bug resolve

--- src/components/Headertop.tsx
+++ src/components/Headertop.tsx
@@ -1,33 +1,24 @@
-/* eslint-disable react/jsx-key */
 /* eslint-disable @next/next/no-img-element */
 import slide1 from "../images/downloaded/1.jpg";
-import slide2 from "../images/downloaded/3d-render-modern-home-office.jpg";
-import slide3 from "../images/downloaded/5966920_20959.jpg";
-
+import { LuPhoneCall } from "react-icons/lu";
 function Headertop() {
-  // const carouselImages = [
-  //   {
-  //     imageUrl: slide1.src, // Use slide1.src instead of slide1 directly
-  //     text: "First slide",
-  //   },
-  //   {
-  //     imageUrl: slide2.src, // Use slide2.src instead of slide2 directly
-  //     text: "Second slide",
-  //   },
-  //   {
-  //     imageUrl: slide3.src, // Use slide3.src instead of slide3 directly
-  //     text: "Third slide",
-  //   },
-  // ];
+
 
   return (
-    <div>        
-              <img
-                src={slide1.src}
-                className="d-block carousels"
-                alt=".."
-              />
-              <div></div>
+    <div> 
+      <div className="headbar">
+        <div className="container">
+        <div className="inner_text_headbar1"><LuPhoneCall size={20} color="white"/>Talk with us</div>
+        <div className="inner_text_headbar2"></div>
+        <div className="inner_text_headbar3"></div>
+        <div className="inner_text_headbar4"></div>
+        </div>
+      </div>
+        <img
+          src={slide1.src}
+          className="d-block carousels"
+          alt=".."
+        />
     </div>
   );
 }
